f53cca49c5922973e64522347c8c01ac98f4f5cf
rameshkorla <rameshkorla.udemy@gmail.com>
2024-08-19 20:37:38 +0530

    update

--- src/main/java/com/wizards/factory/DriverFactory.java
+++ src/main/java/com/wizards/factory/DriverFactory.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverFactory {
 	
-	private WebDriver driver;	
+	public WebDriver driver;	
 	public Properties prop;
 	public WebDriverWait wait;
 	
